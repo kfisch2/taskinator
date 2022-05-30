3110e69fd7935baa70f3b80d2b145458ff6a6db3
kfisch2 <kmlindsey24@gmail.com>
2022-05-29 19:15:43 -0700

    add task type selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
+var tasksToDoEl = document.querySelector("#tasks-to-do")
 // variable to target the button by id
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 // variable for list; can be global
 var listEl = document.querySelector(".task-list");  
-
+// target user input in form
+var input = document.querySelector("#userInput")
 
 // function that creates new task item
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+  // prevents the browswer form automatically refreshing after hitting submit
+  event.preventDefault();
+  
   // variable for creating item in list; must be local to createTask function
   var listItemEl = document.createElement('li');
-  // var input = userInput.value
-  listItemEl.textContent = ('')// input
+  // the textContent of list item is the value of the userInput
+  listItemEl.textContent = input.value
   // add css style by className
   listItemEl.className = "task-item"
   // add li child element to ul parent element
@@ -17,4 +22,4 @@ var createTaskHandler = function() {
 };
 
 // when user clicks button, new task created
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
